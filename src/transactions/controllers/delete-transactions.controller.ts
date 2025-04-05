fbf46d2837004472bbcf0a53fe25f349031242d8
GuiCardilho <gui_cardilho@hotmail.com>
2025-04-05 16:59:24 -0300

    fix: update delete transactions controller to use ApiTags and rename route to 'transacao'

--- src/transactions/controllers/delete-transactions.controller.ts
+++ src/transactions/controllers/delete-transactions.controller.ts
@@ -1,8 +1,9 @@
 import { Controller, Delete } from '@nestjs/common';
-import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { DeleteTransactionsService } from '../services/delete-transactions.service';
 
-@Controller('transactions')
+@ApiTags('transactions')
+@Controller('transacao')
 export class DeleteTransactionsController {
   constructor(
     private readonly deleteTransactionsService: DeleteTransactionsService,
